e67cb6ad7caa3d0c5cb26c62f23850c02aa3b7c4
Matthew Philps <philps.matthew@gmail.com>
2019-12-03 07:51:38 +0900

    wrote and passed test for GET hard coded data

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+
+const { guns } = require("./seedGuns");
+
 const setupExpressServer = () => {
   return app;
 };
 
-app.get("/hello", (req, res) => {
-  res.send("world");
+app.get("/", (req, res) => {
+  res.send({ results: guns });
 });
 
 module.exports = {
--- index.spec.js
+++ index.spec.js
@@ -1,13 +1,15 @@
 const request = require("supertest");
+
+const { guns } = require("./seedGuns");
 const { setupExpressServer } = require("./index");
 
 const app = setupExpressServer();
 
 describe("The API server", () => {
-  describe("GET /hello - returning text", () => {
-    it('should return text/html "world"', async () => {
-      const res = await request(app).get("/hello");
-      expect(res.text).toBe("world");
+  describe("GET /", () => {
+    it("should return a list of guns", async () => {
+      const res = await request(app).get("/");
+      expect(JSON.parse(res.text)).toEqual({ results: guns });
     });
   });
 });
